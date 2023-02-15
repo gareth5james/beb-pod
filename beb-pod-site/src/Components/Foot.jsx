96558dc45c1b323ff5525e3c3c10f934b24a1c4d
gareth5james <gareth5james@yahoo.co.uk>
2023-02-15 08:52:10 +0000

    add fb logo

--- beb-pod-site/src/Components/Foot.jsx
+++ beb-pod-site/src/Components/Foot.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
+import fb from "../resources/Facebook.png"
 
 function Foot() {
-    return <Link to="/contact" className="footer__link">
-        <footer className="footer">
+    return <footer className="footer">
             <p className="footer__buzz">No waiting list <span className="buzz__span">|</span> All ages <span className="buzz__span">|</span> All conditions <span className="buzz__span">|</span> New patients always welcome <span className="buzz__span">|</span> No GP referral necessary</p>
-            <p>annjames.bebpod@btinternet.com<br/>
-            0151 608 0927 (24hrs)</p>
+            <a target="blank" href="https://www.facebook.com/bebingtonpodiatry"><img id="fb" src={fb} alt="facebook logo"></img></a>
+            <Link to="/contact" className="footer__link"><p>annjames.bebpod@btinternet.com<br/>
+            0151 608 0927 (24hrs)</p></Link>
     </footer>
-    </Link>
+   
 }
 
 export default Foot;
